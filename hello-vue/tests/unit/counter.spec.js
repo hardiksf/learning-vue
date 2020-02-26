fc75bccbe49af92c31638abc5bcc87a7f52fb671
Hardik Patel <hpatel@wsgc.com>
2020-02-26 12:52:58 -0800

    Adds dummy test

--- hello-vue/tests/unit/counter.spec.js
+++ hello-vue/tests/unit/counter.spec.js
@@ -3,6 +3,14 @@ import Counter from "@/components/Counter.vue";
 import Vue from "vue";
 
 describe(`Counter`, () => {
-  const wrapper = mount(Counter);
+    const wrapper = mount(Counter);
+
+    function sum(a, b) {
+        return a + b;
+    }
+
+    test('adds 1 + 2 to equal 3', () => {
+        expect(sum(1, 2)).toBe(3);
+    });
 
 });
--- hello-vue/tests/unit/example.spec.js
+++ hello-vue/tests/unit/example.spec.js
@@ -1,3 +1,12 @@
 import { shallowMount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
 
+describe("HelloWorld.vue", () => {
+    function sum(a, b) {
+        return a + b;
+    }
+
+    test('adds 1 + 2 to equal 3', () => {
+        expect(sum(1, 2)).toBe(3);
+    });
+});
